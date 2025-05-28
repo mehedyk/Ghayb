646d72a5bd7c74e8c6d6438e6b1a9b08f158dfc0
S.M. Mehedy Kawser <mahdi.kawser@gmail.com>
2025-05-28 20:59:22 +0600

    Update network.rs

--- Legacy Version (Egui-based)/src/network.rs
+++ Legacy Version (Egui-based)/src/network.rs
@@ -1,1 +1,22 @@
+use reqwest::Client;
+use std::error::Error;
 
+pub async fn check_url(url: &str) -> Result<String, Box<dyn Error>> {
+    let client = Client::builder()
+        .use_rustls_tls()
+        .danger_accept_invalid_certs(true)
+        .build()?;
+
+    let response = client.get(format!("https://{}", url))
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        Ok("Successfully reached the domain".to_string())
+    } else {
+        Ok(format!(
+            "Failed to reach the domain. Status: {}",
+            response.status()
+        ))
+    }
+}
